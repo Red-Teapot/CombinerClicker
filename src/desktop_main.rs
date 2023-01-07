8240d64a466ea0b0b1f7dba2bcd42e470c00f110
Nikita Medvedev <some.redteapot@gmail.com>
2023-01-07 18:07:23 +0700

    Update title screen, add game version number

--- src/desktop_main.rs
+++ src/desktop_main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem="windows"]
+
 use bevy::prelude::App;
 
 fn main() {
--- src/title.rs
+++ src/title.rs
@@ -1,68 +1,103 @@
 use std::time::Duration;
 use bevy::math::vec3;
 use bevy::prelude::*;
-use bevy::ui::Val::Percent;
+use bevy::ui::FocusPolicy;
 use bevy_tweening::{Animator, EaseFunction, RepeatCount, RepeatStrategy, Tween};
-use bevy_tweening::lens::{TextColorLens, TransformPositionLens};
+use bevy_tweening::lens::{TextColorLens, TransformPositionLens, TransformScaleLens, UiPositionLens};
 use iyes_loopless::prelude::*;
 use crate::assets::*;
 use crate::{BackgroundInteraction, GameState, palette};
 
-const FADE_OUT_TIME: f32 = 0.2;
+const FADE_OUT_TIME: f32 = 0.4;
 
-pub fn startup_title(mut commands: Commands, game_assets: Res<GameAssets>) {
-    commands.spawn(Text2dBundle {
-        text: Text::from_section("One Clicker", TextStyle {
-            font: game_assets.font.clone(),
-            font_size: 64.0,
-            color: palette::DARK_BLUE,
-        }).with_alignment(TextAlignment {
-            horizontal: HorizontalAlign::Center,
-            vertical: VerticalAlign::Center,
-        }),
-        transform: Transform::from_xyz(0.0, 200.0, 0.0),
+pub fn startup_title(
+    mut commands: Commands,
+    game_assets: Res<GameAssets>,
+) {
+    commands.spawn(NodeBundle {
+        style: Style {
+            position_type: PositionType::Absolute,
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::SpaceAround,
+            ..default()
+        },
+        focus_policy: FocusPolicy::Pass,
         ..default()
-    }).insert(TitleHint);
+    }).with_children(|container| {
+        container.spawn(TextBundle {
+            text: Text::from_section("One Clicker", TextStyle {
+                font: game_assets.font.clone(),
+                font_size: 64.0,
+                color: palette::DARK_BLUE,
+            }).with_alignment(TextAlignment {
+                horizontal: HorizontalAlign::Center,
+                vertical: VerticalAlign::Center,
+            }),
+            transform: Transform::from_xyz(0.0, 200.0, 0.0),
+            ..default()
+        });
 
-    commands.spawn(Text2dBundle {
-        text: Text::from_section("Click to start", TextStyle {
-            font: game_assets.font.clone(),
-            font_size: 48.0,
-            color: palette::BLUE,
-        }).with_alignment(TextAlignment {
-            horizontal: HorizontalAlign::Center,
-            vertical: VerticalAlign::Center,
-        }),
-        ..default()
-    }).insert(Animator::new(Tween::new(
-        EaseFunction::QuadraticInOut,
-        Duration::from_secs(1),
-        TransformPositionLens {
-            start: vec3(0.0, -10.0, 0.0),
-            end: vec3(0.0, 10.0, 0.0),
-        }
-    ).with_repeat_strategy(RepeatStrategy::MirroredRepeat).with_repeat_count(RepeatCount::Infinite)))
-        .insert(TitleHint);
+        container.spawn(TextBundle {
+            text: Text::from_section("Click to start", TextStyle {
+                font: game_assets.font.clone(),
+                font_size: 48.0,
+                color: palette::BLUE,
+            }).with_alignment(TextAlignment {
+                horizontal: HorizontalAlign::Center,
+                vertical: VerticalAlign::Center,
+            }),
+            ..default()
+        }).insert(
+            Animator::new(Tween::new(
+                EaseFunction::QuadraticInOut,
+                Duration::from_secs(1),
+                UiPositionLens {
+                    start: UiRect::top(Val::Px(-10.0)),
+                    end: UiRect::top(Val::Px(10.0)),
+                })
+                    .with_repeat_strategy(RepeatStrategy::MirroredRepeat)
+                    .with_repeat_count(RepeatCount::Infinite)));
 
-    commands.spawn(Text2dBundle {
-        text: Text::from_section("Game by RedTeapot\nMade for Bevy Jam #2", TextStyle {
-            font: game_assets.font.clone(),
-            font_size: 32.0,
-            color: palette::LIGHT_BROWN,
-        }).with_alignment(TextAlignment {
-            horizontal: HorizontalAlign::Center,
-            vertical: VerticalAlign::Center,
-        }),
-        transform: Transform::from_xyz(0.0, -180.0, 0.0),
-        ..default()
+        container.spawn(TextBundle {
+            text: Text::from_section("Game by RedTeapot\nMade for Bevy Jam #2", TextStyle {
+                font: game_assets.font.clone(),
+                font_size: 32.0,
+                color: palette::LIGHT_BROWN,
+            }).with_alignment(TextAlignment {
+                horizontal: HorizontalAlign::Center,
+                vertical: VerticalAlign::Center,
+            }),
+            transform: Transform::from_xyz(0.0, -180.0, 0.0),
+            ..default()
+        }).insert(TitleHint);
+
+        let version_str = format!("Version {} (post-jam)", env!("CARGO_PKG_VERSION"));
+        container.spawn(TextBundle {
+            text: Text::from_section(version_str.as_str(), TextStyle {
+                font: game_assets.font.clone(),
+                font_size: 32.0,
+                color: palette::LIGHT_BLUE,
+            }),
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: UiRect::new(Val::Px(8.0), Val::Undefined, Val::Undefined, Val::Px(8.0)),
+                ..default()
+            },
+            focus_policy: FocusPolicy::Pass,
+            ..default()
+        });
     }).insert(TitleHint);
 }
 
-pub fn handle_title_click(mut commands: Commands,
-                          interactions: Query<&Interaction, With<BackgroundInteraction>>,
-                          hints: Query<(Entity, &Transform, &Text), With<TitleHint>>,
-                          mut fade_out: Option<ResMut<TitleFadeOut>>,
-                          time: Res<Time>) {
+pub fn handle_title_click(
+    mut commands: Commands,
+    interactions: Query<&Interaction, With<BackgroundInteraction>>,
+    hints: Query<Entity, With<TitleHint>>,
+    mut fade_out: Option<ResMut<TitleFadeOut>>,
+    time: Res<Time>,
+) {
     for interaction in interactions.iter() {
         match interaction {
             Interaction::Clicked if fade_out.is_none() => {
@@ -71,31 +106,16 @@ pub fn handle_title_click(mut commands: Commands,
                 commands.insert_resource(TitleFadeOut {
                     timer: Timer::new(fade_out_time, TimerMode::Once),
                 });
-                for (hint, transform, text) in hints.iter() {
+
+                for hint in hints.iter() {
                     let mut hint_commands = commands.entity(hint);
 
-                    let start_position: Vec3 = transform.translation;
-                    let end_position = start_position + vec3(0.0, 100.0, 0.0);
-                    let start_color: Color = text.sections[0].style.color;
-
-                    hint_commands
-                        .remove::<Animator<Transform>>()
-                        .remove::<Animator<Text>>()
-                        .insert(Animator::new(Tween::new(
+                    hint_commands.insert(Animator::new(Tween::new(
                             EaseFunction::CubicOut,
                             fade_out_time,
-                            TransformPositionLens {
-                                start: start_position,
-                                end: end_position,
-                            }
-                        )))
-                        .insert(Animator::new(Tween::new(
-                            EaseFunction::CubicOut,
-                            fade_out_time,
-                            TextColorLens {
-                                start: start_color,
-                                end: Color::NONE,
-                                section: 0,
+                            UiPositionLens {
+                                start: UiRect::left(Val::Percent(0.0)),
+                                end: UiRect::left(Val::Percent(-100.0)),
                             }
                         )));
                 }
@@ -114,7 +134,7 @@ pub fn handle_title_click(mut commands: Commands,
             if timer.finished() {
                 commands.remove_resource::<TitleFadeOut>();
 
-                for (hint, _, _) in hints.iter() {
+                for hint in hints.iter() {
                     commands.entity(hint).despawn_recursive();
                 }
 
